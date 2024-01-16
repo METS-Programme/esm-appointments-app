523b09fb28ed268d11d621f4cc2f9f02dd24b88d
jabahum <jabajnan8@gmail.com>
2024-01-16 12:37:23 +0300

    fix lint

--- __mocks__/address.mock.ts
+++ __mocks__/address.mock.ts
@@ -1,8 +1,8 @@
 export const mockAddress = {
-  postalCode: '12345',
-  address1: '123 Main St',
-  cityVillage: 'City',
-  stateProvince: 'State',
-  country: 'Country',
+  postalCode: "12345",
+  address1: "123 Main St",
+  cityVillage: "City",
+  stateProvince: "State",
+  country: "Country",
   preferred: true,
 };
